532f3882e4b9db202dccf948e4cf16c66486ca7c
dome <dome@DOME-PC>
2016-06-08 18:44:28 +0200

    test

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int test = 0;
+	cout << "Hallo";
+	cin >> test;
+    return 0;
+}
+
